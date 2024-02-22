06bdcfadb6305ef8f81698a3b9513034ff8766f9
takatoshi.tenko <chanten811@gmail.com>
2024-02-23 00:33:18 +0900

    トップ画面の作成

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceThree")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
-
